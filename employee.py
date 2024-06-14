9d3fcceae286dcf7d1cfbf773b4d81860aa88ce7
amelyap <2215061088@students.unila.ac.id>
2024-06-14 09:11:14 +0700

    video 1

--- employee.py
+++ employee.py
@@ -55,7 +55,7 @@ class Employee:
 
         # upper Frame
         upper_frame=LabelFrame(Main_frame,bd=2,relief=RIDGE, bg='white', text='Employee Information', font=('times new roman',11,'bold'), fg='black')
-        upper_frame.place(x=10,y=10,width=1240,height=150)
+        upper_frame.place(x=10,y=10,width=1240,height=230)
 
         # Label and Entry Fields
         lbl_dep=Label(upper_frame, text='Department:', font=('arial',12,'bold'), bg='white')
@@ -94,13 +94,103 @@ class Employee:
         text_Designition=ttk.Entry(upper_frame, width=22, font=('arial',11,'bold'))
         text_Designition.grid(row=2, column=1, sticky=W, padx=2, pady=7)
 
+        # Married
+        lbl_merried_status=Label(upper_frame, font=("arial",12,"bold"), text="Married Status:", bg='white')
+        lbl_merried_status.grid(row=2, column=2, sticky=W, padx=2, pady=7)
+
+        com_txt_merried=ttk.Combobox(upper_frame, state="readonly",
+                                                         font=("arial",12, "bold"), width=18)
+        com_txt_merried['value']=("Merried", "Unmerried")
+        com_txt_merried.current(0)
+        com_txt_merried.grid(row=2, column=3, sticky=W, padx=2, pady=7)
+
+        # Dob
+        lbl_dob=Label(upper_frame, font=("arial",12, "bold"), text="DOB:", bg="white")
+        lbl_dob.grid(row=3, column=0, sticky=W, padx=2, pady=7)
+
+        txt_dob=ttk.Entry(upper_frame, width=22, font=('arial',11,'bold'))
+        txt_dob.grid(row=3, column=1, padx=2, pady=7)
+
+        # Doj
+        lbl_doj=Label(upper_frame, font=("arial",12, "bold"), text="DOJ:", bg="white")
+        lbl_doj.grid(row=3, column=2, sticky=W, padx=2, pady=7)
+
+        txt_doj=ttk.Entry(upper_frame, width=22, font=('arial',11,'bold'))
+        txt_doj.grid(row=3, column=3, padx=2, pady=7)
+
+        # Id Proof
+        com_txt_proof=ttk.Combobox(upper_frame, state="readonly",
+                                                         font=("arial",12, "bold"), width=18)
+        com_txt_proof['value']=("Select ID Proof", "PAN CARD", "ADHAR CARD", "DRIVING LICENCE")
+        com_txt_proof.current(0)
+        com_txt_proof.grid(row=2, column=0, sticky=W, padx=2, pady=7)
+
+        txt_proof=ttk.Entry(upper_frame, width=22, font=('arial',11,'bold'))
+        txt_proof.grid(row=4, column=1, padx=2, pady=7)
+
+        # gender
+        lbl_gender=Label(upper_frame, font=("arial",12,"bold"), text="Gender:", bg='white')
+        lbl_gender.grid(row=4, column=2, sticky=W, padx=2, pady=7)
+
+        com_txt_gender=ttk.Combobox(upper_frame, state="readonly",
+                                                         font=("arial",12, "bold"), width=18)
+        com_txt_gender['value']=("Male", "Female", "Other")
+        com_txt_gender.current(0)
+        com_txt_gender.grid(row=4, column=3, sticky=W, padx=2, pady=7)
+
+        # phone
+        lbl_phone=Label(upper_frame, font=("arial",12, "bold"), text="Phone No:", bg="white")
+        lbl_phone.grid(row=0, column=4, sticky=W, padx=2, pady=7)
+
+        txt_phone=ttk.Entry(upper_frame, width=22, font=('arial',11,'bold'))
+        txt_phone.grid(row=0, column=5, padx=2, pady=7)
+
+        # country
+        lbl_country=Label(upper_frame, font=("arial",12, "bold"), text="Country:", bg="white")
+        lbl_country.grid(row=1, column=4, sticky=W, padx=2, pady=7)
+
+        txt_country=ttk.Entry(upper_frame, width=22, font=('arial',11,'bold'))
+        txt_country.grid(row=1, column=5, padx=2, pady=7)
+
+        # CTC
+        lbl_ctc=Label(upper_frame, font=("arial",12, "bold"), text="Salary(CTC):", bg="white")
+        lbl_ctc.grid(row=2, column=4, sticky=W, padx=2, pady=7)
+
+        txt_ctc=ttk.Entry(upper_frame, width=22, font=('arial',11,'bold'))
+        txt_ctc.grid(row=2, column=5, padx=2, pady=7)
+
+        # mask image
+        img_mask=Image.open('college_images/pekerja.jpeg')
+        img_mask=img_mask.resize((220,220), Image.LANCZOS)
+        self.photomask=ImageTk.PhotoImage(img_mask)
+
+        self.img_mask=Label(img_frame, image=self.photomask)
+        self.img_mask.place(x=1000, y=0, width=220, height=220)
+
+        # Button Frame
+        button_frame=Frame(upper_frame,bd=2,relief=RIDGE, bg='white')
+        button_frame.place(x=1290,y=10,width=170,height=210)
+
+        btn_add=Button(button_frame,text="Save",font=("arial",15, "bold"), width=13, bg='blue',fg='white')
+        btn_add.grid(row=0, column= 0, padx=1, pady=5)
+
+        btn_update=Button(button_frame,text="Update",font=("arial",15, "bold"), width=13, bg='blue',fg='white')
+        btn_update.grid(row=1, column= 0, padx=1, pady=5)
+
+        btn_delete=Button(button_frame,text="Delete",font=("arial",15, "bold"), width=13, bg='blue',fg='white')
+        btn_delete.grid(row=2, column= 0, padx=1, pady=5)
+
+        btn_clear=Button(button_frame,text="Clear",font=("arial",15, "bold"), width=13, bg='blue',fg='white')
+        btn_clear.grid(row=3, column= 0, padx=1, pady=5)
 
         # down Frame
         down_frame=LabelFrame(Main_frame,bd=2,relief=RIDGE, bg='white', text='Employee Information Table', font=('times new roman',11,'bold'), fg='black')
-        down_frame.place(x=10,y=160,width=1240,height=150)
+        down_frame.place(x=10,y=250,width=1240,height=180)
 
         # Label and Entry Fields
         self.var_dep=StringVar()
+
+
 
 if __name__=="__main__":
     root = Tk()
